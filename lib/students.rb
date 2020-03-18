1bf21c2e564fa809cab6f60a79961a9ed1ebdb6e
astronomical-reducer-9741 <vinh.v.tran@ucdenver.edu>
2020-03-18 01:34:06 +0000

    Automatically backed up by Learn

--- lib/students.rb
+++ lib/students.rb
@@ -6,14 +6,17 @@ def highest_student_gpa
 end
 
 def lowest_student_gpa
-  "SELECT MIN(grade) FROM students;"
+  "SELECT MIN(gpa) FROM students;"
 end
 
 def average_student_gpa
+  "SELECT AVG(gpa) FROM students"
 end
 
 def total_tardies_for_all_students
+  "SELECT SUM(tardies) FROM students"
 end
 
 def average_gpa_for_9th_grade
+  "SELECT AVG(gpa) FROM students WHERE grade = 9"
 end
