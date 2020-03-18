6fe79eba0a81bb3f9cc6e51acc734542069328e1
astronomical-reducer-9741 <vinh.v.tran@ucdenver.edu>
2020-03-18 01:31:34 +0000

    Automatically backed up by Learn

--- lib/students.rb
+++ lib/students.rb
@@ -1,7 +1,8 @@
 ## Code your solution below. Note that your SQL queries should be in quotation marks. 
 
 def highest_student_gpa
-  SELECT MAX(grade) FROM students;
+  temp = SELECT MAX(grade) FROM students;
+  temp
 end
 
 def lowest_student_gpa
